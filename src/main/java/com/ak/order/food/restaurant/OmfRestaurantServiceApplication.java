ac18920d66cad8d174d98178ed56e15ce4552574
Akhtar ali <akthar.ali@embitel.com>
2021-10-21 20:25:11 +0530

    first commit

---
+++ src/main/java/com/ak/order/food/restaurant/OmfRestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ak.order.food.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmfRestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmfRestaurantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak/order/food/restaurant/OmfRestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.order.food.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmfRestaurantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
